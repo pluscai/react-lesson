b409bf2eb0b99dc9e0839cdaf46609c008a42b3f
pluscai <3114658034@qq.com>
2020-08-10 15:54:47 +0800

    5-3 使用Antd实现TodoList页面布局

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,89 +1,31 @@
-import React,{ Component, Fragment } from 'react';
-import TodoItem from "./TodoItem";
-import axios from 'axios';
-import './style.css'
+import React,{ Component } from 'react';
+import 'antd/dist/antd.css';
+import { Input, Button, List} from 'antd';
+
+const data = [
+    'Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed commoner.',
+    'Australian walks 100km after outback crash.',
+    'Man charged over missing wedding girl.',
+    'Los Angeles battles huge wildfires.',
+];
 
 class TodoList extends Component{
-
-    constructor(props){
-        super(props);
-        this.state = {
-            inputValue: '',
-            list: []
-        };
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleBtnClick = this.handleBtnClick.bind(this);
-        this.handleItemDelete = this.handleItemDelete.bind(this);
-    }
-
-
     render() {
         return (
-            <Fragment>
+            <div style={{margin: '10px'}}>
                 <div>
-                    <label htmlFor="insertArea">输入要添加的内容：</label>
-                    <input
-                        className='input'
-                        id='insertArea'
-                        value={this.state.inputValue}
-                        onChange={this.handleInputChange}
-                    />
-                    <button onClick={this.handleBtnClick}>提交</button>
+                    <Input placeholder='todo info' style={{width: '300px',marginRight: '10px'}}></Input>
+                    <Button type="primary">submit</Button>
                 </div>
-                <ul ref={(ul) => {this.ul = ul}}>
-                    { this.getTodoItem() }
-                </ul>
-            </Fragment>
+                <List
+                    style={{marginTop: '10px',width: '300px'}}
+                    bordered
+                    dataSource={data}
+                    renderItem={item => <List.Item>{item}</List.Item>}
+                />
+            </div>
         )
-    }
-
-    componentDidMount() {
-        axios.get('/api/todolist.json')
-            .then((res) => {
-                this.setState(() => ({
-                    list: [...res.data]
-                }));
-            })
-            .catch(() => {alert('error')})
-    }
-
-    getTodoItem() {
-        return this.state.list.map((item, index) => {
-            return (
-                <TodoItem
-                    key={index}
-                    content={item}
-                    index={index}
-                    deleteItem={this.handleItemDelete}
-                >
-                </TodoItem>
-            )
-        })
-    }
-
-    handleInputChange(e) {
-        const value = e.target.value;
-        this.setState(() => ({
-            inputValue: value
-        }));
-    }
-
-    handleBtnClick() {
-        this.setState((prevState) => ({
-            list: [...prevState.list, prevState.inputValue],
-            inputValue: ''
-        }),() => {
-            //console.log(this.ul.querySelectorAll('li').length);
-        })
-
-    }
-
-    handleItemDelete(index) {
-        this.setState((prevState) => {
-            const list = [...prevState.list];
-            list.splice(index, 1);
-            return {list}
-        })
     }
 
 }
