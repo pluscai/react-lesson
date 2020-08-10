52c84de97ae00df31f75a5a4f1176f24161e70a0
pluscai <3114658034@qq.com>
2020-08-10 10:30:34 +0800

    4-11 css过渡

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoList from './TodoList';
+import App from './App';
 
 
-ReactDOM.render(<TodoList />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
