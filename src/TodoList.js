64d77b0f06200a8da57c16f381e907f4f1e7cae5
pluscai <3114658034@qq.com>
2020-08-11 09:09:54 +0800

    5-5&6 Action和Reducer的编写、删除功能

--- src/TodoList.js
+++ src/TodoList.js
@@ -8,23 +8,54 @@ class TodoList extends Component{
     constructor(props) {
         super(props);
         this.state = store.getState();
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleStoreChange = this.handleStoreChange.bind(this);
+        this.handleBtnClick = this.handleBtnClick.bind(this);
+        store.subscribe(this.handleStoreChange)
     }
 
     render() {
         return (
             <div style={{margin: '10px'}}>
                 <div>
-                    <Input value={ this.state.inputValue } placeholder='todo info' style={{width: '300px',marginRight: '10px'}}></Input>
-                    <Button type="primary">submit</Button>
+                    <Input onChange={ this.handleInputChange } value={ this.state.inputValue } placeholder='todo info' style={{width: '300px',marginRight: '10px'}}></Input>
+                    <Button type="primary" onClick={this.handleBtnClick}>submit</Button>
                 </div>
                 <List
                     style={{marginTop: '10px',width: '300px'}}
                     bordered
                     dataSource={this.state.list}
-                    renderItem={item => <List.Item>{item}</List.Item>}
+                    renderItem={(item, index) => <List.Item onClick={this.handleItemDelete.bind(this, index)}>{item}</List.Item>}
                 />
             </div>
         )
+    }
+
+    handleInputChange(e) {
+        const action = {
+            type: 'change_input_value',
+            value: e.target.value
+        };
+        store.dispatch(action)
+    }
+
+    handleStoreChange() {
+        this.setState(store.getState())
+    }
+
+    handleBtnClick() {
+        const action = {
+            type: 'add_todo_item'
+        };
+        store.dispatch(action)
+    }
+
+    handleItemDelete(index) {
+        const action = {
+            type: 'delete_todo_item',
+            index
+        };
+        store.dispatch(action)
     }
 
 }
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -4,5 +4,22 @@ const defaultState = {
 }
 
 export default (state = defaultState, action) => {
+    if( action.type === 'change_input_value') {
+        const newState = JSON.parse(JSON.stringify(state)); // 对象的深拷贝
+        newState.inputValue = action.value;
+        return newState
+    }
+    if( action.type === 'add_todo_item') {
+        const newState = JSON.parse(JSON.stringify(state)); // 对象的深拷贝
+        newState.list.push(newState.inputValue);
+        newState.inputValue = '';
+        return newState
+    }
+    if( action.type === 'delete_todo_item') {
+        const newState = JSON.parse(JSON.stringify(state)); // 对象的深拷贝
+        newState.list.splice(action.index, 1);
+        return newState
+    }
+
     return state
 }
