9206a4e6c728e161bde40b6c9e1172723ab96862
pluscai <3114658034@qq.com>
2020-08-12 15:00:29 +0800

    6-8&9&10 如何使用React-redux完成 TodoList的功能

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,69 +1,59 @@
-import React,{ Component } from 'react';
-import 'antd/dist/antd.css';
-import store from "./store";
-import {
-    getInitList,
-    getInputChangeAction,
-    getAddItemAction,
-    getDeleteItemAction,
-} from './store/actionCreators';
-import TodoListUI from './TodoListUI';
+import React from "react";
+import { connect } from 'react-redux'
 
-
-class TodoList extends Component{
-
-    constructor(props) {
-        super(props);
-        this.state = store.getState();
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleStoreChange = this.handleStoreChange.bind(this);
-        this.handleBtnClick = this.handleBtnClick.bind(this);
-        this.handleItemDelete = this.handleItemDelete.bind(this);
-        store.subscribe(this.handleStoreChange)
-    }
-
-    render() {
-        return (
-            <TodoListUI
-                handleInputChange={this.handleInputChange}
-                inputValue={this.state.inputValue}
-                list={this.state.list}
-                handleBtnClick={this.handleBtnClick}
-                handleItemDelete={this.handleItemDelete}
-            />
-        )
-    }
-
-    componentDidMount() {
-        const action = getInitList();
-        store.dispatch(action);
-        /*axios.get('/api/todolist.json').then((res) => {
-            const data = res.data;
-            const action = initListAction(data);
-            store.dispatch(action)
-        })*/
-
-    }
-
-    handleInputChange(e) {
-        const action = getInputChangeAction(e.target.value)
-        store.dispatch(action)
-    }
-
-    handleStoreChange() {
-        this.setState(store.getState())
-    }
-
-    handleBtnClick() {
-        const action = getAddItemAction()
-        store.dispatch(action)
-    }
-
-    handleItemDelete(index) {
-        const action = getDeleteItemAction(index);
-        store.dispatch(action)
-    }
-
+const TodoList = (props) => {
+    const { inputValue, list, changeInputValue, handleClick, handleDelete} = props;
+    return (
+        <div>
+            <div>
+                <input value={inputValue} onChange={ changeInputValue } />
+                <button onClick={ handleClick }>提交</button>
+            </div>
+            <ul>
+                {
+                    list.map((item, index) => {
+                        return <li key={ index } onClick={handleDelete.bind(this, index)}>{item}</li>
+                    })
+                }
+            </ul>
+        </div>
+    )
 }
 
-export default TodoList;
+const mapStateToProps = (state) => {
+    return {
+        inputValue: state.inputValue,
+        list: state.list
+    }
+}
+
+// store.dispatch ,props
+const mapDispatchToProps = (dispatch) => {
+    return {
+        changeInputValue(e) {
+            const value = e.target.value;
+            const action = {
+                type: 'change_input_value',
+                value
+            }
+            dispatch(action)
+        },
+
+        handleClick() {
+            const action = {
+                type: 'add_item'
+            }
+            dispatch(action)
+        },
+
+        handleDelete(index) {
+            const action = {
+                type: 'delete_item',
+                index
+            }
+            dispatch(action)
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,32 +1,23 @@
-import { CHANGE_INPUT_value, ADD_TODO_ITEM, DELETE_TODO_ITEM, INIT_LIST_ACTION } from './actionTypes';
-
 const defaultState = {
     inputValue: '',
     list: []
 }
-
 export default (state = defaultState, action) => {
-    if( action.type === INIT_LIST_ACTION) {
-        const newState = JSON.parse(JSON.stringify(state)); // 对象的深拷贝
-        newState.list = action.data;
-        return newState
-    }
-    if( action.type === CHANGE_INPUT_value) {
-        const newState = JSON.parse(JSON.stringify(state)); // 对象的深拷贝
+    if(action.type === 'change_input_value') {
+        const newState = JSON.parse(JSON.stringify(state));
         newState.inputValue = action.value;
         return newState
     }
-    if( action.type === ADD_TODO_ITEM) {
-        const newState = JSON.parse(JSON.stringify(state)); // 对象的深拷贝
+    if(action.type === 'add_item') {
+        const newState = JSON.parse(JSON.stringify(state));
         newState.list.push(newState.inputValue);
         newState.inputValue = '';
         return newState
     }
-    if( action.type === DELETE_TODO_ITEM) {
-        const newState = JSON.parse(JSON.stringify(state)); // 对象的深拷贝
-        newState.list.splice(action.index, 1);
+    if(action.type === 'delete_item') {
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.list.splice(action.index, 1)
         return newState
     }
-
-    return state
+    return state;
 }
